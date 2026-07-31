6a0e928f44d823c345d75b42af13b01044104030
ShunKUN1182 <2250378@ecc.ac.jp>
2026-07-31 15:16:01 +0900

    絵文字

---
+++ js/sample06-2.js
@@ -0,0 +1,40 @@
+const todo = document.querySelector(".📝");
+const modal = document.querySelector(".🪟");
+
+const createConfirm = (modal) => {
+    const messageWrap = modal.querySelector(".🪟__💬");
+
+    const show = (message = "") => {
+        messageWrap.textContent = message;
+        modal.showModal();
+        return new Promise((resolve) => {
+            // ダイアログのOKボタンをクリックした時
+            const okBtn = modal.querySelector(".🪟__🔘--✅");
+            okBtn.addEventListener("click", () => {
+                modal.close();
+                resolve(true);
+            });
+            //ダイアログのキャンセルボタンをクリック
+            const ngBtn = modal.querySelector(".🪟__🔘--❌");
+            ngBtn.addEventListener("click", () => {
+                modal.close();
+                resolve(false);
+            });
+        });
+    };
+
+    return { show };
+};
+const confirm = createConfirm(modal);
+
+todo.addEventListener("click", (e) => {
+    const element = e.target;
+
+    if (
+        element.classList.contains("📝__🗑️") ||
+        element.classList.contains("material-symbols-outlined")
+    ) {
+        const todoItemWrap = element.closest(".📝__📄");
+        confirm.show("このアイテムを削除しますか？");
+    }
+});
